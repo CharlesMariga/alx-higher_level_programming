86d27845c840ff8764b079cb4e43ed599c0f4754
charlesmariga <charlesmariga37@gmail.com>
2022-06-02 17:04:47 +0300

    Modified 102-maig_calculation.py file

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-from dis import dis
-
-
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
     if a < b:
@@ -12,6 +9,3 @@ def magic_calculation(a, b):
     else:
         sub(a, b)
         return
-
-
-dis(magic_calculation)
