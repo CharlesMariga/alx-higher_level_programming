3dc6a5f029e5bca932fa9be79e96a0b866675b8a
charlesmariga <charlesmariga37@gmail.com>
2022-06-22 10:34:51 +0300

    Modified 6-square.py file

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -15,10 +15,6 @@ class Square:
         self.__size = size
         self.__position = position
 
-    def area(self):
-        """Calculate the are of a square"""
-        return self.__size ** 2
-
     @property
     def size(self):
         """Retrieves the size property"""
@@ -37,17 +33,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-    def my_print(self):
-        """Prints a square"""
-        if self.__size == 0:
-            print()
-        else:
-            [print("") for i in range(0, self.__position[1])]
-            for i in range(0, self.__size):
-                [print(" ", end="") for j in range(0, self.__position[0])]
-                [print("#", end="") for k in range(0, self.__size)]
-                print("")
-
     @property
     def position(self):
         """Retries the position property"""
@@ -62,3 +47,18 @@ class Square:
                 not all(num > -1 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
+
+    def area(self):
+        """Calculate the are of a square"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Prints a square"""
+        if self.__size == 0:
+            print()
+        else:
+            [print("") for i in range(0, self.__position[1])]
+            for i in range(0, self.__size):
+                [print(" ", end="") for j in range(0, self.__position[0])]
+                [print("#", end="") for k in range(0, self.__size)]
+                print("")
