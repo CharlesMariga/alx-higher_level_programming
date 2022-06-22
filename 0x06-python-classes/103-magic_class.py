2f4d14f76dfea1833e276d5f2f229c70adbbe99e
charlesmariga <charlesmariga37@gmail.com>
2022-06-22 15:12:00 +0300

    Pythone bytecode

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import math
+
+
+class MagiClass:
+    def __init__(self, radius):
+        self.__radius = None
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+        else:
+            self.__radius = radius
+
+    def area(self):
+        return (self.__radius ** 2) * math.pi
+
+    def circumference(self):
+        return (2 * math.pi) * self.__radius
